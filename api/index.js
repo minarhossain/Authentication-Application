4d4f9e811c87c33e1cf1884667ed558250222af3
minarhossain <codewithmnr@gmail.com>
2023-11-07 00:28:44 +0600

    mongodb database connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv"
+
+dotenv.config();
+
 const app = express();
 
 const PORT = 5000;
@@ -9,6 +14,15 @@ app.get('/', (req, res) => {
     res.status(200).json({ Name: "Minar Hossain", status: "Express Server started" })
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is Running http://localhost:${PORT}`)
-})
+async function main() {
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        console.log("Connected DB")
+        app.listen(PORT, () => {
+            console.log(`Server is Running http://localhost:${PORT}`)
+        })
+    } catch (error) {
+        console.log("Connection Error", error);
+    }
+}
+main()
